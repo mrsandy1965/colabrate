93fbc7506d7ca8fc2ab8166ed6dd48e8613ce68f
mrsandy1965 <8055sandesh8055@gmail.com>
2025-02-03 13:03:39 +0530

    interesting question

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
-let obj = new Object();
-obj.z="z";
-obj.k="k";
-obj.a="a";
-console.log(obj);
-console.log(Object.fromEntries(Object.entries(obj).sort((a,b) => a[0].localeCompare(b[0]))));
+// let obj = new Object();
+// obj.z="z";
+// obj.k="k";
+// obj.a="a";
+// console.log(obj);
+// console.log(Object.fromEntries(Object.entries(obj).sort((a,b) => a[0].localeCompare(b[0]))));
+
+function sortEntries(objectToSort , sortBy ,sortOrder){
+    if (sortBy === "key"){
+        if (sortOrder === "desc"){
+        return Object.fromEntries(Object.entries(objectToSort).sort((a,b) => b[0].localeCompare(a[0])));
+        }else{
+        return Object.fromEntries(Object.entries(objectToSort).sort((a,b) => a[0].localeCompare(b[0])));
+        }
+        
+    } else if (sortBy === "value"){
+        if (sortOrder === "desc"){
+        return Object.fromEntries(Object.entries(objectToSort).sort((a,b) => b[1].localeCompare(a[1])));
+        }else{
+        return Object.fromEntries(Object.entries(objectToSort).sort((a,b) => a[1].localeCompare(b[1])));
+        }
+    }
+}
+let favFruit ={
+    fruit : "Mango",
+    region : "India" ,
+    color : "Yellow"
+}
+console.log(sortEntries(favFruit,"value","desc"))
+console.log(sortEntries(favFruit,"value","asc"))
+console.log(sortEntries(favFruit,"key","desc"))
+console.log(sortEntries(favFruit,"key","asc"))
